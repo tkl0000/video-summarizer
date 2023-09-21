a899e8d7bcc1b6aa04c3375b73455d59d4e2422b
tkl0000 <thomasli2025@gmail.com>
2023-09-21 19:29:56 -0400

    just added a bunch fo stuff U know hahahahah

--- summarize.py
+++ summarize.py
@@ -2,25 +2,29 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import urllib.request
 import time
 
+def downloadVideo(video_url):
+    filename = "video1.mp4"
+    urllib.request.urlretrieve(video_url, filename)
 
-def getHtml(video_url):
+def getHtml(canvas_url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=0,0")
     service = Service(chrome_options=chrome_options)
     browser=webdriver.Chrome(service=service)
-    browser.get(video_url)
+    browser.get(canvas_url)
     html = browser.page_source
     browser.close()
     return html
 
-def transcribe_video(video_url):
-    html = getHtml(video_url)
+def transcribe_video(canvas_url):
+    html = getHtml(canvas_url)
     soup = BeautifulSoup(html, "html.parser")
-    video = soup.find_all("source")[0].get("src")
-    print(video)
+    video_url = soup.find_all("source")[0].get("src")
+    
 
 def main():
     url = "https://hcpss.instructuremedia.com/embed/f7c70668-3ace-430c-946b-0d8be1034e6e"
