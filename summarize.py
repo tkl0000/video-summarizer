b2d1f1fd031c0fc7683fb702c39a7ea06db9218e
tkl0000 <thomasli2025@gmail.com>
2023-09-21 15:12:35 -0400

    pull video url from canvas

--- summarize.py
+++ summarize.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 
@@ -7,7 +8,9 @@ import time
 def getHtml(video_url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    browser=webdriver.Chrome(chrome_options=chrome_options)
+    chrome_options.add_argument("--window-size=0,0")
+    service = Service(chrome_options=chrome_options)
+    browser=webdriver.Chrome(service=service)
     browser.get(video_url)
     html = browser.page_source
     browser.close()
@@ -15,9 +18,9 @@ def getHtml(video_url):
 
 def transcribe_video(video_url):
     html = getHtml(video_url)
-    soup = BeautifulSoup()
-
-
+    soup = BeautifulSoup(html, "html.parser")
+    video = soup.find_all("source")[0].get("src")
+    print(video)
 
 def main():
     url = "https://hcpss.instructuremedia.com/embed/f7c70668-3ace-430c-946b-0d8be1034e6e"
