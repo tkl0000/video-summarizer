920dbe827b150006c9d613dabb2563d63acd501b
tkl0000 <thomasli2025@gmail.com>
2023-09-22 13:59:06 -0400

    transcription

--- summarize.py
+++ summarize.py
@@ -4,12 +4,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import urllib.request
 import time
+import whisper
 
-def downloadVideo(video_url):
-    filename = "video1.mp4"
-    urllib.request.urlretrieve(video_url, filename)
-
-def getHtml(canvas_url):
+def download_audio(canvas_url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=0,0")
@@ -18,17 +15,20 @@ def getHtml(canvas_url):
     browser.get(canvas_url)
     html = browser.page_source
     browser.close()
-    return html
-
-def transcribe_video(canvas_url):
-    html = getHtml(canvas_url)
     soup = BeautifulSoup(html, "html.parser")
     video_url = soup.find_all("source")[0].get("src")
-    
+    filename = "video1.mp3"
+    urllib.request.urlretrieve(video_url, filename)
+
+def transcribe_audio():
+    model = whisper.load_model("medium.en")
+    result = model.transcribe("cigs.mp3")
+    print(result["text"])
 
 def main():
     url = "https://hcpss.instructuremedia.com/embed/f7c70668-3ace-430c-946b-0d8be1034e6e"
-    transcribe_video(url)
+    # download_video(url)
+    transcribe_audio()
 
 if (__name__ == "__main__"):
     main()
