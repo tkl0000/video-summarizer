bb30b392e8c068d957c334c0372b53e67ebf05c8
tkl0000 <thomasli2025@gmail.com>
2023-09-24 22:44:41 -0400

    I think it works...

--- summarize.py
+++ summarize.py
@@ -10,7 +10,7 @@ import openai
 def get_key():
     with open("key.txt", "r") as r:
         return r.read()
-
+    
 #returns file name
 def download_audio(canvas_url):
     chrome_options = Options()
@@ -37,14 +37,28 @@ def transcribe_audio(filename):
 
 def summarize(text):
     openai_key = get_key()
-
-    return text
+    openai.api_key = openai_key
+    prompt = "Convert this into notes optimal for studying: \n" + text
+    MODEL = "gpt-3.5-turbo"
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0,
+    )
+    response_text = response['choices'][0]['message']['content']
+    return response_text
 
 def main():
+    start_time = time.time()
     url = "https://hcpss.instructuremedia.com/embed/f7c70668-3ace-430c-946b-0d8be1034e6e"
     audio_file = download_audio(url)
-    text = transcribe_audio("cigs.mp3")
+    text = transcribe_audio(audio_file)
     summarized_text = summarize(text)
+    end_time = time.time()
+    print(f'Elapsed: {end_time - start_time} s')
     print(summarized_text)
 
 if (__name__ == "__main__"):
